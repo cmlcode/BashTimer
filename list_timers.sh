a90b7a9c125144f5abda8eabe93cacd42efa4033
Christopher Lewis <61091285+cmlcode@users.noreply.github.com>
2024-03-03 12:40:37 -0600

    Add timer command

--- list_timers.sh
+++ list_timers.sh
@@ -3,13 +3,13 @@ tracker_file="$(pwd)/.active_timers.txt"
 curr_timer=0
 clean_tracker_file(){
   while IFS=: read -r start_time wait_time PID; do
-    if ! ps -p  $PID > /dev/null 2>&1; then
+    if ! ps -p  ${PID} > /dev/null 2>&1; then
       # PID not found, remove from tracker_file
-      sed -i "/:$PID$/d" "$tracker_file"
+      sed -i "/:$PID$/d" "${tracker_file}"
     else
       curr_time=$(date +%s)
       time_left=$((wait_time - (curr_time - start_time)))
-      echo "Timer $curr_timer: $time_left"
+      echo "Timer ${curr_timer}: ${time_left}s"
       ((curr_timer+=1))
     fi
   done <"$tracker_file"
--- timer.sh
+++ timer.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
 tracker_file="$(pwd)/.active_timers.txt"
-
+help_text="Help text"
 is_uint() {
   case $1 in
     '' | *[!0-9]*) return 1;;
     *) return 0;;
   esac ;
 }
-get_input() {
-# Check if val is a num
+process_input() {
+  if [ $# -eq 0 ]; then
+    echo help_text
+    exit 1
+  fi
+  # Check if val is a num
   is_uint "$1"
   if [ $? -eq 0 ]; then
-    echo "I run"
     (sh ./create_timer.sh "$1")
-  else
-    echo "Input is not an unsigned int"
+    return 0
   fi
+  # Check what command is being run
+  case "$1" in 
+    -c)
+      (sh ./create_timer.sh "$2")
+      ;;
+    -l)
+      sh ./list_timers.sh
+      ;;
+  esac
 }
+process_input "$@"
 exit 0
