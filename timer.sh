03e707098d2823bef672662bb3a3ac3deeef5652
Christopher Lewis <61091285+cmlcode@users.noreply.github.com>
2024-03-11 13:19:14 -0500

    Added help

--- timer.sh
+++ timer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tracker_file="$(dirname "$0")/TimerHelper/.active_timers.txt"
-help_text="Help text"
+help_text="Create Timer: timer <numMins>, timer -c <numMins>\nRemove Timer: timer -r <timerNum>\nList Timers: timer -l\n"
 is_uint() {
   case $1 in
     '' | *[!0-9]*) return 1;;
@@ -9,7 +9,7 @@ is_uint() {
 }
 process_input() {
   if [ $# -eq 0 ]; then
-    echo help_text
+    echo Command not found. Run $0 -h for a command list.
     exit 1
   fi
   # Check if val is a num
@@ -21,13 +21,27 @@ process_input() {
   # Check what command is being run
   case "$1" in 
     -c)
+      if [ $# -eq 1 ]; then
+        echo "Usage: $0 $1 <numMins>"
+        exit 1
+      fi
       (sh $(dirname "$0")/TimerHelper/create_timer.sh "$2")
       ;;
     -l)
       sh $(dirname "$0")/TimerHelper/list_timers.sh
       ;;
     -r)
+      if [ $# -eq 1 ]; then
+        echo "Usage: $0 $1 <timerNum>"
+        exit 1
+      fi
       sh $(dirname "$0")/TimerHelper/remove_timer.sh "$2"
+      ;;
+    -h)
+      printf "$help_text"
+      ;;
+    *)
+      echo "Command not found. Run $0 -h for a command list."
       ;;
   esac
 }
